70d3ba0143471d8d35af3a9d314cee9f7e9bd8e6
Rithesh Bhaskara <rithesh.bhaskara@honeywell.com>
2025-02-03 08:57:51 +0530

    Parmeters

--- features/steps/login.py
+++ features/steps/login.py
@@ -7,7 +7,7 @@ from features.pages.login import Login
 
 @when(u'user launches the website')
 def step_impl(context):
-    context.driver = webdriver.Chrome()
+    # context.driver = webdriver.Chrome()
     context.driver.get('https://www.saucedemo.com/')
     context.driver.implicitly_wait(30)
 
@@ -17,14 +17,14 @@ def step_impl(context):
     Login(context.driver).verify_login_page()
 
 
-@when(u'user enters the username "standard_user"')
-def step_impl(context):
-    Login(context.driver).enter_username('standard_user')
+@when(u'user enters the username "{username}"')
+def step_impl(context, username):
+    Login(context.driver).enter_username(username)
 
 
-@when(u'user enters the password "secret_sauce"')
-def step_impl(context):
-    Login(context.driver).enter_password('secret_sauce')
+@when(u'user enters the password "{password}"')
+def step_impl(context, password):
+    Login(context.driver).enter_password(password)
 
 
 @when(u'user clicks on the login button')
@@ -35,4 +35,3 @@ def step_impl(context):
 @then(u'home page is displayed')
 def step_impl(context):
     Home(context.driver).verify_home_page()
-    time.sleep(3)
